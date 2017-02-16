9b2e60175fff9f79f931932f1ff8805fa992fcd6
aarontang <aarontang@tencent.com>
2017-02-16 17:17:00 +0800

    [ex8]finish "Collaborative Filtering Gradient"

--- machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -42,8 +42,8 @@ Theta_grad = zeros(size(Theta));
 
 J = 1/2 * sum(sum(((X * Theta') .* R - (Y .* R)) .^ 2));
 
-
-
+X_grad = (X * Theta' - Y) .* R * Theta;
+Theta_grad = ((X * Theta' - Y) .* R)' * X;
 
 
 
